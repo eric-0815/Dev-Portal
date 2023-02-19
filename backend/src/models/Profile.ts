cc81dc98a84b9510cd9bf104ef617b2165bb12b6
eric-0815 <67636157+eric-0815@users.noreply.github.com>
2023-02-19 20:20:34 +0800

    Create Profile.ts

---
+++ backend/src/models/Profile.ts
@@ -0,0 +1,159 @@
+import mongoose from "mongoose";
+
+export interface Profile {
+    user: mongoose.Schema.Types.ObjectId;
+    company: string;
+    website: string;
+    location: string;
+    status: string;
+    skills: string[];
+    bio: string;
+    githubusername: string;
+    experience: Experience[];
+    education: Education[];
+    date: Date;
+    social: Social;
+}
+
+interface Education {
+    school: string;
+    degree: string;
+    fieldofstudy: string;
+    from: Date;
+    to: Date;
+    current: boolean;
+    description: string;
+}
+
+interface Experience {
+    title: string;
+    company: string;
+    location: string;
+    from: Date;
+    to: Date;
+    current: boolean;
+    description: string;
+}
+
+interface Social {
+    youtube: string;
+    twitter: string;
+    facebook: string;
+    linkedin: string;
+    instagram: string;
+}
+
+
+const ProfileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    company: {
+        type: String
+    },
+    website: {
+        type: String
+    },
+    location: {
+        type: String
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    skills: {
+        type: [String],
+        required: true
+    },
+    bio: {
+        type: String
+    },
+    githubusername: {
+        type: String
+    },
+    experience: [
+        {
+            title: {
+                type: String,
+                required: true
+            },
+            company: {
+                type: String,
+                required: true
+            },
+            location: {
+                type: String
+            },
+            from: {
+                type: Date,
+                required: true
+            },
+            to: {
+                type: Date
+            },
+            current: {
+                type: Boolean,
+                default: false
+            },
+            description: {
+                type: String
+            }
+        }
+    ],
+    education: [
+        {
+            school: {
+                type: String,
+                required: true
+            },
+            degree: {
+                type: String,
+                required: true
+            },
+            fieldofstudy: {
+                type: String,
+                required: true
+            },
+            from: {
+                type: Date,
+                required: true
+            },
+            to: {
+                type: Date
+            },
+            current: {
+                type: Boolean,
+                default: false
+            },
+            description: {
+                type: String
+            }
+        }
+    ],
+    social: {
+        youtube: {
+            type: String
+        },
+        twitter: {
+            type: String
+        },
+        facebook: {
+            type: String
+        },
+        linkedin: {
+            type: String
+        },
+        instagram: {
+            type: String
+        }
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Profile = mongoose.model<Profile>("User", ProfileSchema);
+
+export default Profile
