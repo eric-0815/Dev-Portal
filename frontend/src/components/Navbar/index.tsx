9fa7a400d1ad513022b2307049a243062bf07307
Eric Hong <eric.hong@intelicare.com.au>
2023-02-22 16:04:17 +0800

    Update index.tsx

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,13 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <nav className="navbar bg-dark">
       <h1>
-        <a href="/"><i className="fas fa-code"></i> DevBlogger</a>
+        <Link to="/"><i className="fas fa-code"></i> DevBlogger </Link>
       </h1>
       <ul>
-        <li><a href="profiles">Developers</a></li>
-        <li><a href="register">Register</a></li>
-        <li><a href="login">Login</a></li>
+        <li><Link to="!#">Developers</Link></li>
+        <li><Link to="register">Register</Link></li>
+        <li><Link to="login">Login</Link></li>
       </ul>
     </nav>
   )
