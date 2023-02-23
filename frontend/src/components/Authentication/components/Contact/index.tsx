9c1a6fc8daf791ee9b8dbe4cd24f0a078a7268e4
eric-0815 <67636157+eric-0815@users.noreply.github.com>
2023-02-23 21:31:04 +0800

    redux practice

---
+++ frontend/src/components/Authentication/components/Contact/index.tsx
@@ -0,0 +1,20 @@
+import { decrement, increment } from "../../../../slices/counterSlice";
+import {
+  useAppDispatch,
+  useAppSelector,
+} from "../../../../store/configureStore";
+
+const Contact = () => {
+  const dispatch = useAppDispatch(); //useDispatch();
+  const { data, title } = useAppSelector((state) => state.counter); //useSelector((state: CounterState) => state);
+  return (
+    <div className="container">
+      <h5>{title}</h5>
+      <h2>{data.toString()}</h2>
+      <button onClick={() => dispatch(decrement(1))}>Decrement</button>
+      <button onClick={() => dispatch(increment(1))}>Increment</button>
+    </div>
+  );
+};
+
+export default Contact;
