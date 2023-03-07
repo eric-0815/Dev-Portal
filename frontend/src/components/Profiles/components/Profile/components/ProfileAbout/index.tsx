2e4fe1b1712238c375e0479a3b95db4492b00572
eric-0815 <67636157+eric-0815@users.noreply.github.com>
2023-03-07 22:53:06 +0800

    scss

--- frontend/src/components/Profiles/components/Profile/components/ProfileAbout/index.tsx
+++ frontend/src/components/Profiles/components/Profile/components/ProfileAbout/index.tsx
@@ -4,7 +4,7 @@ const ProfileAbout = ({ profile }: any) => {
   const { bio, skills, user } = profile;
 
   return (
-    <div className="profile-about bg-light p-2">
+    <div className="profile__about bg-light p-2">
       {bio && (
         <>
           <h2 className="text-primary">
@@ -15,7 +15,7 @@ const ProfileAbout = ({ profile }: any) => {
         </>
       )}
       <h2 className="text-primary">Skill Set</h2>
-      <div className="skills">
+      <div className="profile__about__skills">
         {skills.map((skill: any, index: number) => (
           <div key={index} className="p-1">
             <i className="fas fa-check" /> {skill}
--- frontend/src/components/Profiles/components/Profile/components/ProfileTop/index.tsx
+++ frontend/src/components/Profiles/components/Profile/components/ProfileTop/index.tsx
@@ -1,17 +1,16 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 const ProfileTop = ({ profile }: any) => {
   const { status, company, location, website, user } = profile;
   return (
-    <div className="profile-top bg-primary p-2">
+    <div className="profile__top bg-primary p-2">
       <img className="round-img my-1" src={user?.avatar} alt="" />
       <h1 className="large">{user?.name}</h1>
       <p className="lead">
         {status} {company ? <span> at {company}</span> : null}
       </p>
       <p>{location ? <span>{location}</span> : null}</p>
-      <div className="icons my-1">
+      <div className="profile__top__icons my-1">
         {website ? (
           <a href={website} target="_blank" rel="noopener noreferrer">
             <i className="fas fa-globe fa-2x" />
@@ -37,10 +36,6 @@ const ProfileTop = ({ profile }: any) => {
       </div>
     </div>
   );
-};
-
-ProfileTop.propTypes = {
-  profile: PropTypes.object.isRequired,
 };
 
 export default ProfileTop;
