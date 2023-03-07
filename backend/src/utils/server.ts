b0314d393581144f68d9801903ead2e122512d79
Eric Hong <eric.hong@intelicare.com.au>
2023-03-07 14:16:40 +0800

    cors

--- backend/src/utils/server.ts
+++ backend/src/utils/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors'
 
 const createServer = () => {
     const app = express();
@@ -6,6 +7,15 @@ const createServer = () => {
     // Init Middleware
     app.use(express.json())
 
+    app.use(
+        cors({
+          allowedHeaders: ["x-auth-token", "Content-Type"], // you can change the headers
+          exposedHeaders: ["x-auth-token"], // you can change the headers
+          origin: "*",
+          methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+          preflightContinue: false
+        })
+      );
 
     return app
 }
