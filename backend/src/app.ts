73e8ab5dc0f580a98dc51360285d7c5bf1b4dfec
eric-0815 <67636157+eric-0815@users.noreply.github.com>
2023-03-16 21:11:59 +0800

    updated swagger and cors setting

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -21,7 +21,7 @@ const options = {
         },
         servers: [
             {
-                url: 'http://localhost:5000'
+                url: 'https://dev-center-backend.onrender.com'//'http://localhost:5000'
             }
         ]
     },
