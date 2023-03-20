545ce107302ba59191cde272d6efd001d573070a
Eric Hong <eric.hong@intelicare.com.au>
2023-03-20 09:39:11 +0800

    updated alert stuff

--- frontend/src/components/Alert/index.tsx
+++ frontend/src/components/Alert/index.tsx
@@ -3,11 +3,28 @@ import { AlertState } from "../../slices/alertSlice";
 import { useAppSelector } from "../../store/configureStore";
 
 const Alert = () => {
+
+  const removeDuplicateAlerts = (alerts: AlertState[]): AlertState[] => {
+    const uniqueAlerts: AlertState[] = [];
+    const messages: string[] = [];
+
+    for (const alert of alerts) {
+      if (!messages.includes(alert.msg)) {
+        uniqueAlerts.push(alert);
+        messages.push(alert.msg);
+      }
+    }
+
+    return uniqueAlerts;
+  }
+
   const { alerts } = useAppSelector((state) => state.alertState);
+  const uniqueAlerts = removeDuplicateAlerts(alerts)
+
   return (
     <div className="container">
-      {alerts?.length > 0 &&
-        alerts?.map((alert: AlertState) => (
+      {uniqueAlerts?.length > 0 &&
+        uniqueAlerts?.map((alert: AlertState) => (
           <div key={alert.id} className={`alert alert-${alert.alertType}`}>
             {alert.msg}
           </div>
